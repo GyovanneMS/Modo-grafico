8468a8616814f81f4f4ef45c99e61ee8d45fa661
GYOVANNE MARTINS SABARÁ <22102360@CFP127ALUNO.LOCAL>
2022-03-09 07:59:14 -0300

    Atualização e/ou correção de erros

--- Variavel.java
+++ Variavel.java
@@ -10,8 +10,8 @@ class Variavell {
         jop.showMessageDialog(null, "O primeiro número é: " + N1 + ", e o sgndo é: " + N2);
 
         int N3 = N1;
-        int N1 = N2;
-        int N2 = N3;
+        N1 = N2;
+        N2 = N3;
 
         
         jop.showMessageDialog(null, "Agor o primeiro número é: " + N1 + ", e o sgndo é: " + N2);
