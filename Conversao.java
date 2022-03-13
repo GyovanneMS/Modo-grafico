afbd34d7b4d1853d19834e22b71b59076d367dc2
GyovanneMS <98404327+GyovanneMS@users.noreply.github.com>
2022-03-13 20:19:04 -0300

    Update Conversao.java

--- Conversao.java
+++ Conversao.java
@@ -4,10 +4,10 @@ class Conversao {
     public static void main(String args[]) {
         JOptionPane jop = new JOptionPane();
 
-        double Valor = Double.parseDouble(jop.showInputDialog(null, "O valor a ser convertido: "));
+        double Real = Double.parseDouble(jop.showInputDialog(null, "O valor a ser convertido: "));
         double Dolar = Double.parseDouble(jop.showInputDialog(null, "O valor valor do dolar: "));
 
-        double Vfinal = Valor * Dolar;
+        double Vfinal = Dolar / Real;
 
         jop.showMessageDialog(null, "O valor convertido é: " + Vfinal);
     }
